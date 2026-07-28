2e3e3ccd4fa4ba08dc0400da1d059458e17f7486
Mariana <salinasmariana659@gmail.com>
2026-07-28 10:35:57 -0500

    version 4.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:formularioo/homepage.dart';
 
+void main() {
+  runApp(const Principal());
+}
+
 class Principal extends StatelessWidget {
   const Principal({super.key});
 
