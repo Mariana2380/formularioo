1b1362561e384a4405f571ba6814f9ee0782158c
Mariana <salinasmariana659@gmail.com>
2026-07-28 08:45:17 -0500

    1

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:formularioo/HomePage.dart';
+
+class Principal extends StatefulWidget {
+  const Principal({super.key});
+
+  @override
+  State<Principal> createState() => _PrincipalState();
+}
+
+class _PrincipalState extends State<Principal> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Homepage(), debugShowCheckedModeBanner: false);
+  }
+}
